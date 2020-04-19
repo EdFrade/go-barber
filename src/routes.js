0abae6fd086db2dc839ee5be343a96da23872d1c
Eduardo Frade <eduardofrad@hotmail.com>
2020-04-19 17:16:20 -0300

    Add migration and model users

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/test', (req, res) => res.json({ message: 'Hello Rocke' }));
+routes.get('/users', async (req, res) => {
+  const user = await User.create({
+    name: 'Eduardo Frade Oliveira',
+    email: 'eduardofrad@hotmail.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
