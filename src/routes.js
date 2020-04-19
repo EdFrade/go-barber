79c307ff9589fe6e2e01c85f184374e39034fae2
Eduardo Frade <eduardofrad@hotmail.com>
2020-04-19 19:04:03 -0300

    Add user controller

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/users', async (req, res) => {
-  const user = await User.create({
-    name: 'Eduardo Frade Oliveira',
-    email: 'eduardofrad@hotmail.com',
-    password_hash: '12345678',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
